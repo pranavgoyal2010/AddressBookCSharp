6633d9e69e555add3a1fb32241a1b99ccd5c2d13
Pranav Goyal <pr945005@dal.ca>
2024-02-14 11:18:11 +0530

    [Pranav] Add : created Contact class to represent a contact in Address Book

---
+++ AddressBookCSharp/Contact.cs
@@ -0,0 +1,14 @@
+﻿namespace AddressBookCSharp
+{
+    public class Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Email { get; set; }
+    }
+}
