3e7fa4c5713fae0c4c3ffee565ce0cfc744718e0
Pranav Goyal <pr945005@dal.ca>
2024-02-15 10:43:25 +0530

    [Pranav] Refactor : changed data type to string for phone number

--- AddressBookCSharp/obj/Debug/net8.0/AddressBookCSharp.AssemblyInfo.cs
+++ AddressBookCSharp/obj/Debug/net8.0/AddressBookCSharp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AddressBookCSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6633d9e69e555add3a1fb32241a1b99ccd5c2d13")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89fd71bc28009588a2ac010e5efcd6e2c62dae8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("AddressBookCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AddressBookCSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
