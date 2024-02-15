f245136dad175f772c1f4c485b0618223fbcbbab
Pranav Goyal <pr945005@dal.ca>
2024-02-15 10:37:34 +0530

    [Pranav] Add : adding modified supporting files to repository

--- AddressBookCSharp/obj/Debug/net8.0/AddressBookCSharp.AssemblyInfo.cs
+++ AddressBookCSharp/obj/Debug/net8.0/AddressBookCSharp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AddressBookCSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+957577b890cd38203e06335d415f863b1ad1b1dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+832aa6452090adf419fd88ddbab92aba8ee69362")]
 [assembly: System.Reflection.AssemblyProductAttribute("AddressBookCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AddressBookCSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
