b0629c843b4b5266443054e48a65dcad775262ec
Pranav Goyal <pr945005@dal.ca>
2024-02-15 11:43:22 +0530

    [Pranav] Add : adding code to edit a contact

--- AddressBookCSharp/AddressBookMain.cs
+++ AddressBookCSharp/AddressBookMain.cs
@@ -6,7 +6,11 @@ public class AddressBookMain
     {
         //Console.WriteLine("Welcome to Address Book System");
 
+
+
         List<Contact> addressBook = new List<Contact>();
+
+        //CREATING NEW CONTACT
 
         Contact contact = new Contact();
 
@@ -34,12 +38,109 @@ public class AddressBookMain
         Console.WriteLine("Enter Email : ");
         contact.Email = Console.ReadLine();
 
+
+
+        //ADDING NEW CONTACT
+
         addressBook.Add(contact);
+
+        Console.WriteLine();
 
         foreach (Contact c in addressBook)
         {
             c.PrintContact();
         }
+
+        Console.WriteLine();
+
+        //EDITING CONTACT
+
+        Console.WriteLine("Enter first name and last name to edit");
+
+        string firstName = Console.ReadLine();
+        string lastName = Console.ReadLine();
+        firstName = firstName.ToLower();
+        lastName = lastName.ToLower();
+
+        Contact? contactToEdit = null;
+
+        foreach (Contact c in addressBook)
+        {
+            if ((c.FirstName.ToLower()).Equals(firstName) && (c.LastName.ToLower()).Equals(lastName))
+            {
+                contactToEdit = c;
+                break;
+            }
+        }
+
+        if (contactToEdit == null)
+        {
+            Console.WriteLine("Contact does not exist");
+        }
+        else
+        {
+            Console.WriteLine("Enter option to edit");
+            Console.WriteLine("1. First Name");
+            Console.WriteLine("2. Last Name");
+            Console.WriteLine("3. Address");
+            Console.WriteLine("4. City");
+            Console.WriteLine("5. State");
+            Console.WriteLine("6. Postal Code");
+            Console.WriteLine("7. Phone Number");
+            Console.WriteLine("8. Email");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Enter new First Name");
+                    contactToEdit.FirstName = Console.ReadLine();
+                    Console.WriteLine("First Name updated");
+                    break;
+                case 2:
+                    Console.WriteLine("Enter new Last Name");
+                    contactToEdit.LastName = Console.ReadLine();
+                    Console.WriteLine("Last Name updated");
+                    break;
+                case 3:
+                    Console.WriteLine("Enter new Address");
+                    contactToEdit.Address = Console.ReadLine();
+                    Console.WriteLine("Address Name updated");
+                    break;
+                case 4:
+                    Console.WriteLine("Enter new City");
+                    contactToEdit.City = Console.ReadLine();
+                    Console.WriteLine("City updated");
+                    break;
+                case 5:
+                    Console.WriteLine("Enter new State");
+                    contactToEdit.State = Console.ReadLine();
+                    Console.WriteLine("State updated");
+                    break;
+                case 6:
+                    Console.WriteLine("Enter new Postal Code");
+                    contactToEdit.PostalCode = Console.ReadLine();
+                    Console.WriteLine("Postal code updated");
+                    break;
+                case 7:
+                    Console.WriteLine("Enter new Phone Number");
+                    contactToEdit.PhoneNumber = Console.ReadLine();
+                    Console.WriteLine("Phone Number updated");
+                    break;
+                case 8:
+                    Console.WriteLine("Enter new Email");
+                    contactToEdit.Email = Console.ReadLine();
+                    Console.WriteLine("Email updated");
+                    break;
+                default:
+                    Console.WriteLine("Wrong input try again.");
+                    break;
+            }
+
+            Console.WriteLine();
+            contactToEdit.PrintContact();
+        }
+
     }
 }
 
--- AddressBookCSharp/obj/Debug/net8.0/AddressBookCSharp.AssemblyInfo.cs
+++ AddressBookCSharp/obj/Debug/net8.0/AddressBookCSharp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AddressBookCSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5dd7caa1f3ccf7fb714edbfc69cf775227a16ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90ea8a5d829d4b2ab5ec45abb6ef4f17532b91a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("AddressBookCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AddressBookCSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
